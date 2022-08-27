6034d370e9cef566421347fbb24581726a179b50
Tyler Levine <tyler@bitgo.com>
2022-08-26 21:27:27 -0700

    remove some boilerplate garbage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
 import styles from './App.module.css';
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <h1>Bitcoin Transaction Visualizer</h1>
+      <p>Inspired by the laminated reference sheet you get in Jimmy Song's Programming Bitcoin class</p>
+      <textarea>Paste a transaction hex here</textarea>
     </div>
   );
 };
